aaae34391f552bb6c17c0d168124d9786c539026
liangfeng1234 <101499419+liangfeng1234@users.noreply.github.com>
2023-05-16 10:24:30 +0800

    新增了查询所有分支的方法！
    新增了查询所有分支的方法，接口在BranchController中。

--- src/main/java/com/minigit/controller/BranchController.java
+++ src/main/java/com/minigit/controller/BranchController.java
@@ -3,10 +3,14 @@ package com.minigit.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.minigit.common.R;
 import com.minigit.entity.Branch;
+import com.minigit.entity.Commit;
 import com.minigit.entity.Repo;
+import com.minigit.entity.User;
 import com.minigit.entityService.BranchService;
+import com.minigit.entityService.CommitService;
 import com.minigit.entityService.RepoService;
 import com.minigit.entityService.UserService;
+import com.sun.org.apache.bcel.internal.generic.LNEG;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Map;
+import java.util.PrimitiveIterator;
 
 @Slf4j
 @RestController
@@ -25,6 +30,9 @@ public class BranchController {
     private UserService userService;
     @Autowired
     private RepoService repoService;
+
+    @Autowired
+    private CommitService commitService;
 
 
     @PostMapping("/add")
@@ -48,12 +56,33 @@ public class BranchController {
         Long authorId = (Long) session.getAttribute("user");
         LambdaQueryWrapper<Repo> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(Repo::getAuthorId, authorId).eq(Repo::getName, repoName);
-        Repo repo1 = repoService.getOne(queryWrapper);
+        Repo repo = repoService.getOne(queryWrapper);
 
         LambdaQueryWrapper<Branch> queryWrapper1 = new LambdaQueryWrapper<>();
-        queryWrapper1.eq(Branch::getRepoId,repo1.getId());
+        queryWrapper1.eq(Branch::getRepoId,repo.getId());
         List<Branch> list = branchService.list(queryWrapper1);
         return R.success(list);
     }
 
+    @GetMapping("/{branchName}/commits")
+    public R<List<Commit>> getAllCommits(@PathVariable String userName, @PathVariable String repoName,
+                                         @PathVariable String branchName, HttpSession httpSession){
+        /*LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getAccountName, userName);
+        User user = userService.getOne(queryWrapper);
+        LambdaQueryWrapper<Repo> queryWrapper2 = new LambdaQueryWrapper<>();
+        queryWrapper2.eq(Repo::getAuthorId, user.getId()).eq(Repo::getName, repoName);
+        Repo repo = repoService.getOne(queryWrapper2);
+
+        LambdaQueryWrapper<Branch> queryWrapper3 = new LambdaQueryWrapper<>();
+        queryWrapper3.eq(Branch::getRepoId,repo.getId()).eq(Branch::getName, branchName);
+        Branch branch = branchService.getOne(queryWrapper3);
+
+        LambdaQueryWrapper<Commit> queryWrapper4 = new LambdaQueryWrapper<>();
+        queryWrapper4.eq();*/
+
+        List<Commit> list = commitService.getAllCommits(userName, repoName, branchName);
+
+        return R.success(list);
+    }
 }
--- src/main/java/com/minigit/mapper/CommitMapper.java
+++ src/main/java/com/minigit/mapper/CommitMapper.java
@@ -1,9 +1,19 @@
 package com.minigit.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.minigit.common.R;
 import com.minigit.entity.Commit;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface CommitMapper extends BaseMapper<Commit> {
+    @Select("select * from user  \n" +
+            "     inner join repo on user.id = repo.author_id  \n" +
+            "     inner join branch on branch.repo_id = repo.id  \n" +
+            "     inner join commit on commit.branch_id = branch.id \n" +
+            "     order by commit.create_time ASC;")
+    List<Commit> getAllCommits(String userName, String repoName, String branchName);
 }
